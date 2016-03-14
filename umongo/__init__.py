f63e9de98764e66d1c7e31a2c4c052af236ac9b1
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2016-03-14 17:26:50 +0100

    Bump version: 0.3.4 → 0.4.0

--- umongo/__init__.py
+++ umongo/__init__.py
@@ -19,7 +19,7 @@ from .wrapped_data import EmbeddedDocument
 
 __author__ = 'Emmanuel Leblond'
 __email__ = 'emmanuel.leblond@gmail.com'
-__version__ = '0.3.4'
+__version__ = '0.4.0'
 __all__ = (
     'Document',
     'EmbeddedDocument',
