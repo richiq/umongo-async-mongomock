660e5dbb74ddcb906a07ab4839b8451110f91a0f
Jérôme Lafréchoux <jlafrechoux@nobatek.com>
2016-10-25 12:27:13 +0200

    Don't validate allow_none fields if value is None

--- umongo/data_proxy.py
+++ umongo/data_proxy.py
@@ -145,7 +145,8 @@ class BaseDataProxy:
     def set(self, name, value, to_raise=KeyError):
         name, field = self._get_field(name, to_raise)
         value = field._deserialize(value, name, None)
-        field._validate(value)
+        if getattr(field, 'allow_none', False) is False or value is not None:
+            field._validate(value)
         self._data[name] = value
         self._mark_as_modified(name)
 
