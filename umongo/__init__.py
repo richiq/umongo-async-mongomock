c547804e9c7d6ca98ce5c4f8110932184a534925
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2016-11-02 10:50:51 +0100

    Bump version: 0.10.0 → 0.11.0

--- umongo/__init__.py
+++ umongo/__init__.py
@@ -36,7 +36,7 @@ from .i18n import set_gettext
 
 __author__ = 'Emmanuel Leblond'
 __email__ = 'emmanuel.leblond@gmail.com'
-__version__ = '0.10.0'
+__version__ = '0.11.0'
 __all__ = (
     'Instance',
     'PyMongoInstance',
