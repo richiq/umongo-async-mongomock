f21147b928bdad84961c73399fa00e66b2a18b74
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2016-11-11 20:35:38 +0100

    Bump version: 0.11.0 → 0.12.0

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ requirements = [
 
 setup(
     name='umongo',
-    version='0.11.0',
+    version='0.12.0',
     description="sync/async MongoDB ODM, yes.",
     long_description=readme + '\n\n' + history,
     author="Emmanuel Leblond",
