Scille/umongo

Pass meta attributes to Schema in as_marshmallow_schema
Closes #65

umongo/fields.py
@@ -494,11 +494,13 @@ class EmbeddedField(BaseField, ma_fields.Nested):
         field_kwargs = self._extract_marshmallow_field_params(mongo_world)
         if params:
             nested_params = params.pop('params', None)
+            nested_meta = params.pop('meta', None)
             field_kwargs.update(params)
         else:
             nested_params = None
+            nested_meta = None
         nested_ma_schema = self._embedded_document_cls.schema.as_marshmallow_schema(
-            params=nested_params, mongo_world=mongo_world, **kwargs)
+            params=nested_params, mongo_world=mongo_world, meta=nested_meta, **kwargs)
         return ma_fields.Nested(nested_ma_schema, **field_kwargs)
 
     def _required_validate(self, value):

richiq/umongo-async-mongomock

umongo/fields.py
@@ -496,13 +496,15 @@ class EmbeddedField(BaseField, ma_fields.Nested):
         field_kwargs = self._extract_marshmallow_field_params(mongo_world)
         if params:
             nested_params = params.pop('params', None)
+            nested_meta = params.pop('meta', None)
             field_kwargs.update(params)
         else:
             nested_params = None
+            nested_meta = None
         schema_kwargs = {k: v for k, v in kwargs.items()
                          if k in ('base_schema_cls', 'check_unknown_fields')}
         nested_ma_schema = self._embedded_document_cls.schema.as_marshmallow_schema(
-            params=nested_params, mongo_world=mongo_world, **schema_kwargs)
+            params=nested_params, mongo_world=mongo_world, meta=nested_meta, **schema_kwargs)
         return ma_fields.Nested(nested_ma_schema, **field_kwargs)
 
     def _required_validate(self, value):