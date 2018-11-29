d03391834b2784ee63a024c8fe7844069abf6918
Jérôme Lafréchoux <jlafrechoux@nobatek.com>
2018-11-29 20:43:59 +0100

    Bump version: 0.15.0 → 1.0.0

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ requirements = [
 
 setup(
     name='umongo',
-    version='0.15.0',
+    version='1.0.0',
     description="sync/async MongoDB ODM, yes.",
     long_description=readme + '\n\n' + history,
     author="Emmanuel Leblond",
--- umongo/__init__.py
+++ umongo/__init__.py
@@ -36,7 +36,7 @@ from .i18n import set_gettext
 
 __author__ = 'Emmanuel Leblond'
 __email__ = 'emmanuel.leblond@gmail.com'
-__version__ = '0.15.0'
+__version__ = '1.0.0'
 __all__ = (
     'Instance',
     'PyMongoInstance',
