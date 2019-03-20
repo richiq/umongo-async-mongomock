d41ef462481e0a738e02a71f8994cea6229935ef
Jérôme Lafréchoux <jlafrechoux@nobatek.com>
2019-03-20 18:37:13 +0100

    Don't call _deserialize_from_mongo in EmbeddedDocument._deserialize

--- umongo/fields.py
+++ umongo/fields.py
@@ -441,8 +441,10 @@ class EmbeddedField(BaseField, ma_fields.Nested):
         embedded_document_cls = self.embedded_document_cls
         if isinstance(value, embedded_document_cls):
             return value
+        if not isinstance(value, dict):
+            raise ValidationError({'_schema': ['Invalid input type.']})
         # Handle inheritance deserialization here using `cls` field as hint
-        if embedded_document_cls.opts.offspring and isinstance(value, dict) and 'cls' in value:
+        if embedded_document_cls.opts.offspring and 'cls' in value:
             to_use_cls_name = value.pop('cls')
             if not any(o for o in embedded_document_cls.opts.offspring
                        if o.__name__ == to_use_cls_name):
@@ -455,20 +457,12 @@ class EmbeddedField(BaseField, ma_fields.Nested):
                 raise ValidationError(str(e))
             return to_use_cls(**value)
         else:
-            # `Nested._deserialize` calls schema.load without partial=True
-            data, errors = self.schema.load(value, partial=True)
-            if errors:
-                raise ValidationError(errors, data=data)
-            return self._deserialize_from_mongo(data)
+            return embedded_document_cls(**value)
 
     def _serialize_to_mongo(self, obj):
         return obj.to_mongo()
 
     def _deserialize_from_mongo(self, value):
-        # When this method is called from `_deserialize`, `value` can be
-        # already deserialized, in such a case do nothing.
-        if isinstance(value, self.embedded_document_cls):
-            return value
         return self.embedded_document_cls.build_from_mongo(value)
 
     def _validate_missing(self, value):
