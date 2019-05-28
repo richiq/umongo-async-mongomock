d45c934114245426cd9957f3f5083f686bc5f1df
Jérôme Lafréchoux <jlafrechoux@nobatek.com>
2019-05-28 10:36:05 +0200

    Bump version: 2.0.3 → 2.0.4

--- umongo/__init__.py
+++ umongo/__init__.py
@@ -37,7 +37,7 @@ from .i18n import set_gettext
 
 __author__ = 'Emmanuel Leblond'
 __email__ = 'emmanuel.leblond@gmail.com'
-__version__ = '2.0.3'
+__version__ = '2.0.4'
 __all__ = (
     'missing',
 
