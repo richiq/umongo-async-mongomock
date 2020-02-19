d8672071ee8e9c62b06184cf8ec12e3d6bf02afa
Jérôme Lafréchoux <jlafrechoux@nobatek.inef4.com>
2020-02-19 16:58:43 +0100

    Add python_requires to setup.py

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
     url='https://github.com/touilleMan/umongo',
     packages=['umongo', 'umongo.frameworks'],
     include_package_data=True,
+    python_requires='>=3.5',
     install_requires=requirements,
     extras_require={
         'motor': ['motor>=1.1,<3.0'],
