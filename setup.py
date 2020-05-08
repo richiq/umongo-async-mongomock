9750cef753d9c4c2e8cf0a33420e982fe8e9fb15
Jérôme Lafréchoux <jlafrechoux@nobatek.inef4.com>
2020-05-08 20:35:10 +0200

    Bump version: 3.0.0b6 → 3.0.0b7

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ requirements = [
 
 setup(
     name='umongo',
-    version='3.0.0b6',
+    version='3.0.0b7',
     description="sync/async MongoDB ODM, yes.",
     long_description=readme + '\n\n' + history,
     author="Emmanuel Leblond",
