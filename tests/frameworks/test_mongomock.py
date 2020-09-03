7f2c3d43632337f8b33d365db225e99b6ddc3133
Jérôme Lafréchoux <jlafrechoux@nobatek.inef4.com>
2020-09-03 15:33:15 +0200

    Fix pylint6 compat (skipif requires a string)

--- tests/frameworks/test_mongomock.py
+++ tests/frameworks/test_mongomock.py
@@ -4,12 +4,14 @@ import pytest
 
 from ..common import TEST_DB
 
+DEP_ERROR = 'Missing mongomock'
+
 try:
     from mongomock import MongoClient
 except ImportError:
-    dep_error = 'Missing mongomock module'
+    dep_error = True
 else:
-    dep_error = None
+    dep_error = False
 
 
 if not dep_error:  # Make sure the module is valid by importing it
@@ -26,7 +28,7 @@ def db():
 
 
 # MongoMockBuilder is 100% based on PyMongoBuilder so no need for really heavy tests
-@pytest.mark.skipif(dep_error is not None, reason=dep_error)
+@pytest.mark.skipif(dep_error, reason=DEP_ERROR)
 def test_mongomock(classroom_model):
     Student = classroom_model.Student
     john = Student(name='John Doe', birthday=datetime(1995, 12, 12))
--- tests/frameworks/test_txmongo.py
+++ tests/frameworks/test_txmongo.py
@@ -9,15 +9,15 @@ from pymongo.results import InsertOneResult, UpdateResult, DeleteResult
 
 from ..common import BaseDBTest, TEST_DB, con
 
+DEP_ERROR = 'Missing txmongo or pytest_twisted'
 
 # Check if the required dependencies are met to run this driver's tests
-dep_error = None
 try:
     import pytest_twisted
     from txmongo import MongoConnection
     from twisted.internet.defer import Deferred, inlineCallbacks, succeed
 except ImportError:
-    dep_error = 'Missing txmongo or pytest_twisted'
+    dep_error = True
 
     # Given the test function are generator, we must wrap them into a dummy
     # function that pytest can skip
@@ -32,6 +32,7 @@ except ImportError:
     pytest_inlineCallbacks = skip_wrapper
 else:
     pytest_inlineCallbacks = pytest_twisted.inlineCallbacks
+    dep_error = False
 
 from umongo import Document, EmbeddedDocument, fields, exceptions, Reference
 
@@ -61,7 +62,7 @@ def db():
     return make_db()
 
 
-@pytest.mark.skipif(dep_error is not None, reason=dep_error)
+@pytest.mark.skipif(dep_error, reason=DEP_ERROR)
 class TestTxMongo(BaseDBTest):
 
     @pytest_inlineCallbacks
