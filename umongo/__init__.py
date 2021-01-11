54b3c2c4d2359b9d5f408ebab4e0da48180b3fde
Jérôme Lafréchoux <jlafrechoux@nobatek.inef4.com>
2021-01-11 17:39:15 +0100

    Bump version: 3.0.0b14 → 3.0.0

--- umongo/__init__.py
+++ umongo/__init__.py
@@ -29,7 +29,7 @@ from .i18n import set_gettext
 
 __author__ = 'Emmanuel Leblond, Jérôme Lafréchoux'
 __email__ = 'jerome@jolimont.fr'
-__version__ = '3.0.0b14'
+__version__ = '3.0.0'
 __all__ = (
     'missing',
 
